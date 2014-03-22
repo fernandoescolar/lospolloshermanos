839c6383513182cac52777e363a5b60ff79caf78
fernandoescolar <fer.escolar@gmail.com>
2014-03-22 18:27:45 +0100

    initial commit

---
+++ LosPollosHermanos.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LosPollosHermanos.Web.Startup))]
+namespace LosPollosHermanos.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
