c29ad0dd209ddb73fecaa1543be5d0ed0be64261
acasquete <acasquete@outlook.com>
2014-03-28 15:36:27 +0100

    Stock View

---
+++ LosPollosHermanos.Franchise/Product.xaml.cs
@@ -0,0 +1,57 @@
+﻿using FirstFloor.ModernUI.Windows;
+using LosPollosHermanos.Infrastructure;
+using LosPollosHermanos.ServiceContracts;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace LosPollosHermanos.Franchise
+{
+    /// <summary>
+    /// Interaction logic for Order.xaml
+    /// </summary>
+    public partial class Product : UserControl, IContent
+    {
+        private AvailableProduct product;
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        public void OnFragmentNavigation(FirstFloor.ModernUI.Windows.Navigation.FragmentNavigationEventArgs e)
+        {
+            var productId = int.Parse(e.Fragment);
+            this.Load(productId);
+        }
+
+        public void OnNavigatedFrom(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+        }
+
+        public void OnNavigatedTo(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+            var parts = e.Source.OriginalString.Split('#');
+            var productId = int.Parse(parts[parts.Length - 1]);
+            this.Load(productId);
+        }
+
+        public void OnNavigatingFrom(FirstFloor.ModernUI.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+        }
+
+        private void Load(int productId)
+        {
+            this.product = Stock.AvailableProducts.FirstOrDefault(o => o.Id == productId);
+            this.DataContext = product;
+        }
+
+        private void OnStartClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+        }
+
+        private void OnDoneClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ LosPollosHermanos.Franchise/Stock.xaml.cs
@@ -0,0 +1,53 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using FirstFloor.ModernUI.Windows;
+using LosPollosHermanos.Infrastructure;
+using LosPollosHermanos.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace LosPollosHermanos.Franchise
+{
+    /// <summary>
+    /// Interaction logic for Orders.xaml
+    /// </summary>
+    public partial class Stock : UserControl
+    {
+        private readonly Timer timer;
+
+        internal readonly static List<AvailableProduct> AvailableProducts = new List<AvailableProduct>();
+
+        public Stock()
+        {
+            InitializeComponent();
+            this.timer = new Timer(OnReloadCallback, null, new TimeSpan(0), TimeSpan.FromSeconds(10));
+        }
+
+        private void OnReloadCallback(object state)
+        {
+            this.LoadAvailableProducts();
+            this.LoadLinks();
+        }
+
+        private void LoadAvailableProducts()
+        {
+            using (var proxy = new Proxy<IProductsService>("ProductsService"))
+            {
+                AvailableProducts.AddRange(proxy.Call(s => s.GetAvailableProducts()));
+            }
+        }
+
+        private void LoadLinks()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.tab.Links.Clear();
+                AvailableProducts.Select(o => new Link { DisplayName = o.Name, Source = new Uri("Product.xaml#" + o.Id, UriKind.Relative) }).ToList().ForEach(this.tab.Links.Add);
+            }), DispatcherPriority.Normal, null);
+        }
+    }
+}
