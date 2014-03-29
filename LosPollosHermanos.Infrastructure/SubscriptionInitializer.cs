bfdcbb6486ae0ae7cc0265bdd5532dbd55435e46
fernandoescolar <fer.escolar@gmail.com>
2014-03-29 10:57:03 +0100

    added servicebus helpers

---
+++ LosPollosHermanos.Infrastructure/SubscriptionInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace LosPollosHermanos.Infrastructure
+{
+    public static class SubscriptionInitializer
+    {
+        public static InitializationRequest Initialize()
+        {
+            return new InitializationRequest
+            {
+                Issuer = ConfigurationManager.AppSettings["SERVICE_BUS_ISSUER"],
+                IssuerKey = ConfigurationManager.AppSettings["SERVICE_BUS_ISSUER_KEY"],
+                Namespace = ConfigurationManager.AppSettings["SERVICE_BUS_NAMESPACE"]
+            };
+        }
+    }
+}
